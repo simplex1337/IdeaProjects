66c361d2a6d70dfef84f229675fefb28114f62ef
simplex1337 <cas98@bk.ru>
2017-02-06 17:47:39 +0700

    небольшой рефактор

--- GfD/src/Player1.java
+++ GfD/src/Player1.java
@@ -1,10 +1,10 @@
 /**
  * Created by alex on 06.02.17.
  */
-public class Player {
+public class Player1 {
 
 
-    public Player() {
+    public Player1() {
 
     }
 }
---
+++ GfD/src/Player2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by alex on 06.02.17.
+ */
+public class Player2 {
+
+
+    public Player2() {
+
+    }
+}
