0d2a072e93d27846e221db4327e1dfa382ae2265
gle4er <gle4er97@gmail.com>
2017-02-07 09:20:21 +0700

    Привёл к код стайлу, переосмыслил некоторые моменты

--- GfD/src/Ball.java
+++ GfD/src/Ball.java
@@ -3,44 +3,45 @@ import java.util.Random;
 
 public class Ball {
 
-    private double x, y;
-    private int r, vecx, vecy;
+    private double x;
+    private double y;
+
+    private int r;
+    private int vecx;
+    private int vecy;
+
     private Rectangle bounds;
 
-    public Ball() {}
-    public Ball(Frame frame) {
-        bounds = frame.getBounds();
-        setX(bounds.getCenterX());
-        setY(bounds.getCenterY());
+    public Ball(Rectangle bounds) {
+        setBounds(bounds);
+        setX(bounds.getMaxX() / 2);
+        setY(bounds.getMaxY() / 2);
         Random rnd = new Random(System.currentTimeMillis());
         setVecx(rnd.nextInt(11) - 5);
         setVecy(rnd.nextInt(11) - 5);
         setR(5);
     }
+
     public int getR() { return r; }
+
     public double getX() { return x; }
+
     public double getY() { return y; }
+
     public int getVecy() { return vecy; }
+
     public int getVecx() { return vecx; }
-    private void setR(int r) {
-        if (r > 0 && r < bounds.getHeight() / 2)
-            this.r = r;
-        else
-            this.r = 5;
-    }
-    private void setX(double x) {
-        if (x > bounds.getMinX() && x < bounds.getMaxX())
-            this.x = x;
-        else
-            this.x = bounds.getWidth() / 2;
-    }
-    private void setY(double y) {
-        if (y > bounds.getMinY() && y < bounds.getMaxY())
-            this.y = y;
-        else
-            this.y = bounds.getHeight() / 2;
-    }
+
+    private void setBounds(Rectangle bounds) { this.bounds = bounds; }
+
+    private void setR(int r) { this.r = r; }
+
+    private void setX(double x) { this.x = x; }
+
+    private void setY(double y) { this.y = y; }
+
     private void setVecx(int vecx) { this.vecx = vecx; }
+
     private void setVecy(int vecy) { this.vecy = vecy; }
 
     public void move() {
