4789880c3e00f648f38f21618fb81c7decb61e8e
simplex1337 <cas98@bk.ru>
2018-11-15 12:27:45 +0700

    начал писать раздачу карт

--- crypto4/src/Main.java
+++ crypto4/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Table t = new Table(5);
+//        t.distribution();
+//        t.code_cards(7);
     }
 }
--- crypto4/src/Table.java
+++ crypto4/src/Table.java
@@ -21,10 +21,58 @@ public class Table {
         return players;
     }
 
+    public void distribution() {
+         for(Player player:players) {
+             code_cards(player.getC());
+//             System.out.println();
+         }
+         // раздаем по 2 карты каждому
+         // каждый просит остальных расшифровать карты
+         // потом расшифровывает сам и узнает свои карты
+    }
+
+    public void code_cards(int c) {
+        int[] coded_cards = new int[cards.length];
+        for (int i = 0; i < cards.length; i++) {
+            coded_cards[i] = mod_pow(cards[i], c, p);
+        }
+        shuffleArray(coded_cards);
+        this.cards = coded_cards;
+        for (int i = 0; i < cards.length; i++) {
+            System.out.print(" " + cards[i]);
+        }
+    }
+
+    public static void shuffleArray(int[] a) {
+        int n = a.length;
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    private static void swap(int[] a, int i, int change) {
+        int helper = a[i];
+        a[i] = a[change];
+        a[change] = helper;
+    }
+
+    private int mod_pow(long a, long pow, long mod) {
+        long res = 1;
+        while (pow > 0) {
+            if (pow % 2 == 1)
+                res = (res * a) % mod;
+            a = (a * a) % mod;
+            pow >>= 1;
+        }
+        return (int) res;
+    }
+
     private int[] get_cards() {
         int[] cards = new int[52];
         for (int i = 0; i < 52; i++) {
-            cards[i] = i + 1;
+            cards[i] = i + 2;
         }
         return cards;
     }
